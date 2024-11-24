0e056f8d3e040e0fa6935163edb5b176546b990c
Maria Clara Amorim <mariaclaraamorim221@gmail.com>
2024-11-24 14:04:47 -0300

    feat: add theme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,28 +1,28 @@
 import { createContext, useContext, useState } from "react";
 
 interface ThemeContextType {
-  mainColor: string;
-  setMainColor: React.Dispatch<React.SetStateAction<string>>;
+    mainColor: string;
+    setMainColor: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const ThemeContext = createContext<ThemeContextType | null>(null);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mainColor, setMainColor] = useState("#0ef");
+    const [mainColor, setMainColor] = useState("#0ef");
 
-  return (
-    <ThemeContext.Provider value={{ mainColor, setMainColor }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+    return (
+        <ThemeContext.Provider value={{ mainColor, setMainColor }}>
+            {children}
+        </ThemeContext.Provider>
+    );
 }
 
 export function useTheme() {
-  const context = useContext(ThemeContext);
+    const context = useContext(ThemeContext);
 
-  if (!context) {
-    throw new Error("useTheme deve ser usado dentro de um ThemeProvider");
-  }
+    if (!context) {
+        throw new Error("useTheme deve ser usado dentro de um ThemeProvider");
+    }
 
-  return context;
+    return context;
 }
