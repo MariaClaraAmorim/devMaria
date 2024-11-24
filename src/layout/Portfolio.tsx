8166c1562dce7ccd05c98b338e8286b41db812b4
Maria Clara Amorim <mariaclaraamorim221@gmail.com>
2024-11-24 14:05:14 -0300

    feat: add layout ao projeto

---
+++ src/layout/Portfolio.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../components/header/header';
+import Footer from '../components/footer';
+
+interface LayoutProps {
+    children: React.ReactNode;
+}
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    );
+};
+export default Layout;
